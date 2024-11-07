f77990fb968c8637e9cdc9ac7f93446eb2dbe35b
Jennifer <jenniferve09@gmail.com>
2024-11-07 18:21:58 -0500

    actualizacion en el proyecot

---
+++ database/migrations/2024_11_06_203631_create_vehiculos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->text('descripcion'); // Campo descripcion de tipo texto
+            $table->text('categoria'); // Campo categoria de tipo texto
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+};
